f678c3ac6e38e529a336af9f4554cf2b41597196
AndreasVaage <andrevaa@stud.ntnu.no>
2017-12-06 21:20:08 +0100

    renewed ComputeStagecost after discovering of bugs

--- ComputeStageCosts.m
+++ ComputeStageCosts.m
@@ -58,87 +58,150 @@ function G = ComputeStageCosts( stateSpace, controlSpace, mazeSize, walls, targe
 %           G(i, l) represents the cost if we are in state i and apply
 %           control input l.
 
-% REMARKS
-% What to do if there is a hole on the restart square?
- % What to do if restart square is in disturbance's reach?
+  statesNextToWallTable = createStatesNextToWallsTable(walls, mazeSize);
+    resetCellIdx = getStateIdx(resetCell,mazeSize);
+    G = zeros(size(stateSpace,1),size(controlSpace,1));
+    zeroInputIdx = 1;
+    % i = start state,    cell_i = start cell
+    % j = intended state,      cell_j = intended cell (end cell before noise)
+    % u = controll input, step_u = controll input step 
+    
+    for i = 1:length(stateSpace)
+        cell_i = stateSpace(i,:);
+        if cell_i == targetCell
+            G(i,:) = Inf;
+            G(i,zeroInputIdx) = 0;
+            continue
+        end
+        for u = 1:length(controlSpace)
+            step_u = controlSpace(u,:);
+            if blockedByWall(cell_i,step_u,statesNextToWallTable,mazeSize)
+                G(i,u) = Inf;
+                continue
+            end
+            
+            cell_j = cell_i + step_u;
+            P_fallDuringU = probOfFalling(cell_i,step_u,p_f,holes);
+            P_fallDuringW = 0;
+            P_hitWall = 0;
+            for step_w = controlSpace(1:9,:)'
+                if not(blockedByWall(cell_j,step_w',statesNextToWallTable,mazeSize)) % No Wall
+                    P_fallDuringW = P_fallDuringW +  probOfFalling(cell_j,step_w',p_f,holes);
+                elseif isStateOnHole(cell_j,holes) % Wall, and wanted state is a hole
+                    P_hitWall = P_hitWall + 1/9;
+                    P_fallDuringW = P_fallDuringW + p_f/9;
+                else % Wall, and wanted state is not a hole
+                    P_hitWall = P_hitWall + 1/9;
+                end
+            end
+   
+            gTimeStep = 1;
+            gFallDuringU = c_r * P_fallDuringU;
+            gFallDuringW = c_r * (1-P_fallDuringU)*P_fallDuringW;
+            gHitWall = c_p * (1-P_fallDuringU)*P_hitWall;
 
-% P(i, j, l) = P(transition from i to j if l applied)
-P = ComputeTransitionProbabilities( stateSpace, controlSpace, ...
-    mazeSize, walls, targetCell, holes, resetCell, p_f );
-
-G = zeros(size(stateSpace,1),size(controlSpace,1));
-zeroInputIdx = 1;
-
-for xIdx=1:size(stateSpace,1)
-   for uIdx=1:size(controlSpace,1)
-       x = stateSpace(xIdx,:);
-       u = controlSpace(uIdx,:);
-       xAfterU = x+u;
-       xAfterUIdx = getStateIdx(xAfterU);
-       
-       if all(x == targetCell) && all(u == [0,0])
-           G(xIdx,uIdx) = 0;
-           continue
-       end
-       
-       if xAfterUIdx <= 0 || P(xIdx,xAfterUIdx,uIdx) == 0
-           G(xIdx,uIdx) = Inf;
-           continue;
-       end
-       
-       
-       P_ArriveAtXAfterUandW = P(xIdx,xAfterUIdx,uIdx);
-       P_StayDuringW = P(xAfterUIdx,xAfterUIdx,zeroInputIdx);
-       P_ArriveAtXAfterU = P_ArriveAtXAfterUandW / P_StayDuringW;
-
-       P_FallDuringW = P(xAfterUIdx,getStateIdx(resetCell),zeroInputIdx);
-       P_ZeroW = 1/9;
-       P_HitWallNoFall = P_StayDuringW - P_ZeroW;
-       if isStateOnHole(xAfterU,holes)
-           P_NoFall = 1-p_f;
-       else
-           P_NoFall = 1;
-       end
-       P_HitWall = P_HitWallNoFall/P_NoFall;
-
-       gTimeStep = 1;
-       gFallDuringU = c_r * (1-P_ArriveAtXAfterU);
-       gFallDuringW = c_r * P_FallDuringW;
-       gHitWall = c_p * P_HitWall;
-
-       G(xIdx,uIdx) = gTimeStep + gFallDuringU + P_ArriveAtXAfterU*(gFallDuringW + gHitWall);
-   end 
-end
-
-% Debug
-% for startStateIdx = 1:length(stateSpace)
-%     for controlInputIdx = 1:length(controlSpace) 
-%         s = sum(P(startStateIdx,:,controlInputIdx));
-%         if true%((s < 0.99999 && s ~= 0) || s > 1.0001) %|| isequal(stateSpace(startStateIdx)',targetCell))
-%             %warning('Sum of probabilities is not equal 1');
-%             PlotMazeDebugg( 1, mazeSize, walls, targetCell, holes, resetCell, P,stateSpace,controlSpace,startStateIdx,controlInputIdx);
-%             disp(G(startStateIdx,controlInputIdx))
-%             w = waitforbuttonpress; 
-%             close all
+            G(i,u) = gTimeStep + gFallDuringU + gFallDuringW + gHitWall;
+        end
+    end
+    
+    
+    % Debug:
+    % Plots the probabilities, pauses the program and gives a warning if the
+    % sum of probabilities for a given start state and input is not equal
+    % to 0 or 1.
+%     for i = 1:length(stateSpace)
+%         for u = 1:length(controlSpace) 
+%             s = sum(P(i,:,u));
+%             if ((s < 0.99999 && s ~= 0) || s > 1.0001) %|| isequal(stateSpace(startStateIdx)',targetCell))
+%                 warning('Sum of probabilities is not equal 1');
+%                 PlotMazeDebugg( 1, mazeSize, walls, targetCell, holes, resetCell, P,stateSpace,controlSpace,i,u);
+%                 waitforbuttonpress; 
+%                 close all
+%             end
 %         end
 %     end
-% end
-
-function idx = getStateIdx(coordinate)
-        if all(coordinate > 0) && all(coordinate <= mazeSize)
-            idx = ( coordinate(1) - 1 ) * mazeSize( 2 ) + coordinate(2);
-        else
-            idx = 0;
-        end
 end
 
-end %end ComputeStageCost
+function blocked = blockedByWall(start,u,statesNextToWallTable,mazeSize)
+% Given start state and control input, returns true of the step is 
+% blocked by a wall.
+    newState = start;
+    number_of_steps = max(abs(u));
+    for i = 1:number_of_steps
+        state = newState;
+        newState = state + u/number_of_steps;
+        j = getStateIdx(newState,mazeSize);
+        if j == 0 || statesNextToWallTable(getStateIdx(state,mazeSize),j)
+            blocked = true;
+            return
+        end
+    end
+    blocked = false;
+end
 
+function blocked = createStatesNextToWallsTable(walls, mazeSize)
+    % Create a MN x MN logical array where only two states lying next to 
+    % each other, with a wall between, returns true, rest are false.
+    blocked = false(mazeSize(1)*mazeSize(2));
+    for k = 1:2:size(walls,1)
+        x1 = walls(k,1); y1 = walls(k,2);
+        x2 = walls(k+1,1); y2 = walls(k+1,2);
+        if ((x1-x2) == 0) % Vertical wall
+            for y = (min(y1,y2) + 1):max(y1,y2)
+                for shift = [0 1; 0 -1; 0 0; 1 0; -1 0]'
+                    oneSideOfWall = getStateIdx([x1,y+shift(1)],mazeSize);
+                    otherSideOfWall = getStateIdx([x1+1,y+shift(2)],mazeSize);
+                    % Assignes index 0 if out of bound
+                    if oneSideOfWall ~= 0 && otherSideOfWall ~= 0 
+                        blocked(oneSideOfWall,otherSideOfWall) = true;
+                        blocked(otherSideOfWall,oneSideOfWall) = true;
+                    end
+                end
+            end
+        elseif ((y1 - y2) == 0) % Horisontal wall
+            for x = (min(x1,x2) + 1):max(x1,x2)
+                for shift = [0 1; 0 -1; 0 0; 1 0; -1 0]'
+                    oneSideOfWall = getStateIdx([x+shift(1),y1],mazeSize);
+                    otherSideOfWall = getStateIdx([x+shift(2),y1+1],mazeSize);
+                    if oneSideOfWall ~= 0 && otherSideOfWall ~= 0
+                        blocked(oneSideOfWall,otherSideOfWall) = true;
+                        blocked(otherSideOfWall,oneSideOfWall) = true;
+                    end
+                end
+            end
+        end
+    end
+end
+    
+function idx = getStateIdx(cell, mazeSize)
+% Returns the corresponding state for a given cell. Returns 0 if the cell
+% lies outside the board.
+    if all(cell > 0) && all(cell <= mazeSize)
+        idx = ( cell(1) - 1 ) * mazeSize( 2 ) + cell(2);
+    else
+        idx = 0;
+    end
+end
+
+function prob = probOfFalling(start,u,p_f,holes)
+% Returns the probability of falling while performing the step u from state
+% start.
+    prob = 0;
+    state = start;
+    number_of_steps = max(abs(u));
+    for i = 1:number_of_steps
+        state = state + u/number_of_steps;
+        if isStateOnHole(state,holes) == true
+            prob = prob + (1-prob)*p_f;
+        end
+    end
+end
+    
 function xOnHole = isStateOnHole(x,holes)
+% Returns true if a cell x, lies on a hole.
     if isempty(holes)
         xOnHole = false;
     else
         xOnHole = ismember(x,holes,'rows');
     end
 end
-
