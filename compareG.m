6d104a7e85b3d2ea05691142c96fbae748949df1
AndreasVaage <andrevaa@stud.ntnu.no>
2017-12-12 17:44:28 +0100

    removed debugging

--- compareG.m
+++
@@ -1,8 +0,0 @@
-function  compareG(G1,G2)
-diff = G1 - G2;
-[i,j] = ind2sub(size(diff),find(not(isnan(diff)) & (diff > 0.000001 | diff < -0.000001)));
-disp('Not equal at:')
-disp('  State Controlinput')
-disp([i j])
-end
-
